930435c1d2f95cb00bcbb8a12393b380acd6688f
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2018-05-11 23:35:10 +0800

    added transition

---
+++ resources/assets/js/Transition/no-mode.vue
@@ -0,0 +1,19 @@
+<template>
+    <transition mode="out-in" name="no-mode-translate-fade">
+        <slot></slot>
+    </transition>
+</template>
+<style>
+    .no-mode-translate-fade-enter-active, .no-mode-translate-fade-leave-active {
+        transition: all 0.4s;
+    }
+    .no-mode-translate-fade-enter, .no-mode-translate-fade-leave-active {
+        opacity: 0;
+    }
+    .no-mode-translate-fade-enter {
+        transform: translateX(31px);
+    }
+    .no-mode-translate-fade-leave-active {
+        transform: translateX(-31px);
+    }
+</style>
